211bfd180317a0942d7dc7fc29edccceb9004d6e
MadsKolbye <christiane1662@yahoo.dk>
2021-10-04 20:39:12 +0200

    big commit

---
+++ SDinjections/src/test/java/mads/kea/sdinjections/SDinjectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package mads.kea.sdinjections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SDinjectionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ ScriptTest/src/test/java/kea/mads/scripttest/ScriptTestApplicationTests.java
@@ -0,0 +1,13 @@
+package kea.mads.scripttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScriptTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
